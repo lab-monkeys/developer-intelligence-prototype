e6e770cdf26b5cb16811960bbfd0f1e2c4d879ae
Mark Riggan <5150241+asuwebdesign@users.noreply.github.com>
2023-07-27 12:51:04 -0400

    Added icons to repo selector

--- components/code-repo-selector.jsx
+++ components/code-repo-selector.jsx
@@ -1,33 +1,38 @@
 "use client"
 
 import { useState } from 'react'
-import { Check, ChevronsUpDown } from "lucide-react"
+import { Check, ChevronsUpDown, Github } from "lucide-react"
 
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, } from "@/components/ui/command"
 import { Popover, PopoverContent, PopoverTrigger, } from "@/components/ui/popover"
 
-const frameworks = [
+const repositories = [
   {
     value: "next.js",
     label: "Next.js",
+    icon: Github,
   },
   {
     value: "sveltekit",
     label: "SvelteKit",
+    icon: Github,
   },
   {
     value: "nuxt.js",
     label: "Nuxt.js",
+    icon: Github,
   },
   {
     value: "remix",
     label: "Remix",
+    icon: Github,
   },
   {
     value: "astro",
     label: "Astro",
+    icon: Github,
   },
 ]
 
@@ -40,23 +45,24 @@ export function CodeRepoSelector() {
       <PopoverTrigger asChild>
         <Button variant="outline" role="combobox" aria-expanded={open} className="w-[200px] justify-between">
           {value
-            ? frameworks.find((framework) => framework.value === value)?.label
-            : "Select framework..."}
+            ? repositories.find((repository) => repository.value === value)?.label
+            : "Select repository..."}
           <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
         </Button>
       </PopoverTrigger>
       <PopoverContent className="w-[200px] p-0">
         <Command>
-          <CommandInput placeholder="Search framework..." />
-          <CommandEmpty>No framework found.</CommandEmpty>
+          <CommandInput placeholder="Search repository..." />
+          <CommandEmpty>No repository found.</CommandEmpty>
           <CommandGroup>
-            {frameworks.map((framework) => (
-              <CommandItem key={framework.value} onSelect={(currentValue) => {
+            {repositories.map((repository) => (
+              <CommandItem key={repository.value} onSelect={(currentValue) => {
                 setValue(currentValue === value ? "" : currentValue)
                 setOpen(false)
               }}>
-                <Check className={cn("mr-2 h-4 w-4", value === framework.value ? "opacity-100" : "opacity-0" )}/>
-                {framework.label}
+                <Check className={cn("mr-2 h-4 w-4", value === repository.value ? "opacity-100" : "opacity-0")} />
+                <repository.icon className={cn("mr-2 w-4 h-4", repository.value === value ? "opacity-100" : "opacity-40")} />
+                {repository.label}
               </CommandItem>
             ))}
           </CommandGroup>
