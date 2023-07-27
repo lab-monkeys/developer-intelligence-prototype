0add5ff59ea6a359a458fda5d8300bb706d05b87
Mark Riggan <5150241+asuwebdesign@users.noreply.github.com>
2023-07-27 11:12:19 -0400

    Updated sign in page

--- app/page.js
+++ app/page.js
@@ -1,16 +1,73 @@
-import Image from 'next/image'
-import Link from 'next/link'
+import Image from "next/image"
+import Link from "next/link"
+import { buttonVariants } from "@/components/ui/button"
 
 export const metadata = {
-  title: 'Red Hat Developer Intelligence'
+  title: 'Dashboard - Red Hat Developer Intelligence'
 }
 
-export default function Page() {
+export default function AuthenticationPage() {
   return (
     <>
-    <Image src="/logos/logo--developer-intelligence.svg" alt="Red Hat Developer Intelligence logo" width={330} height={32} />
-    <h1>Red Hat Developer Intelligence</h1>
-    <Link href="/dashboard">Go to dashboard</Link>
+      <div className="md:hidden">
+        <Image
+          src="/examples/authentication-light.png"
+          width={1280}
+          height={843}
+          alt="Authentication"
+          className="block dark:hidden"
+        />
+        <Image
+          src="/examples/authentication-dark.png"
+          width={1280}
+          height={843}
+          alt="Authentication"
+          className="hidden dark:block"
+        />
+      </div>
+      <div className="container relative hidden h-full flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+        
+        <div className="relative hidden h-full flex-col bg-muted p-16 text-white dark:border-r lg:flex">
+          <div className="absolute inset-0 bg-zinc-900" />
+          <div className="relative z-20 flex items-center text-lg font-medium">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="mr-2 h-6 w-6"
+            >
+              <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
+            </svg>
+            Red Hat Developer Intelligence
+          </div>
+          <div className="relative z-20 mt-auto">
+            <blockquote className="space-y-2">
+              <p className="text-lg">
+                &ldquo;This library has saved me countless hours of work and
+                helped me deliver stunning designs to my clients faster than
+                ever before.&rdquo;
+              </p>
+              <footer className="text-sm">Sofia Davis</footer>
+            </blockquote>
+          </div>
+        </div>
+        <div className="lg:p-8">
+          <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+            <div className="flex flex-col space-y-2 text-center">
+              <h1 className="text-2xl font-semibold tracking-tight">Sign in</h1>
+              <p className="text-sm text-muted-foreground">Enter your email below to create your account</p>
+            </div>
+            
+            <Link href="/dashboard" className={buttonVariants()}>Go to dashboard</Link>
+
+            <p className="px-8 text-center text-sm text-muted-foreground">By clicking continue, you agree to our <Link href="#" className="underline underline-offset-4 hover:text-primary">Terms of Service</Link> and <Link href="#" className="underline underline-offset-4 hover:text-primary">Privacy Policy</Link>.</p>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
