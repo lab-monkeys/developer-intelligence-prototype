376e8c93d1fb1ede9d565cdc79df4bb4f86f9c4a
Mark Riggan <5150241+asuwebdesign@users.noreply.github.com>
2023-07-28 11:57:51 -0400

    Added search toggle

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -6,6 +6,7 @@ import { DateRangeSelector } from '@/components/date-range-selector'
 import { UserNav } from '@/components/user-nav'
 import { NotificationsToggle } from '@/components/notifications-toggle'
 import { ChartLeadTime } from '@/components/chart-lead-time'
+import { SearchToggle } from '@/components/search-toggle'
 
 export const metadata = {
   title: 'Dashboard - Red Hat Developer Intelligence'
@@ -18,8 +19,11 @@ export default function Page() {
       <header className="flex justify-between items-center px-16 py-16">
         <Image src="/logos/logo--developer-intelligence.svg" alt="Red Hat Developer Intelligence logo" width={330} height={32} />
         <h1 className="visually-hidden">Red Hat Developer Intelligence</h1>
-        <div className="flex justify-end items-center gap-4">
-          <NotificationsToggle />
+        <div className="flex justify-end items-center gap-8">
+          <div className="flex justify-end items-center gap-2">
+            <SearchToggle />
+            <NotificationsToggle />
+          </div>          
           <UserNav />
         </div>
       </header>
---
+++ components/search-toggle.jsx
@@ -0,0 +1,12 @@
+'use client'
+
+import { Toggle } from '@/components/ui/toggle'
+import { Search } from "lucide-react"
+
+export function SearchToggle() {
+  return (
+    <Toggle aria-label="Toggle search" disabled>
+      <Search className="w-6 h-6" />
+    </Toggle>
+  )
+}
