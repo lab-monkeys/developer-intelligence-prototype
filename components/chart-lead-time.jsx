738c396ce9cdb1c52a016a9e8ed7059c98058600
Mark Riggan <5150241+asuwebdesign@users.noreply.github.com>
2023-07-28 11:58:17 -0400

    Updated attributes for lead time chart

--- components/chart-lead-time.jsx
+++ components/chart-lead-time.jsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle, } from '@/components/ui/card'
-import { XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, ComposedChart, Legend, Area, Line } from 'recharts'
+import { XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, ComposedChart, Legend, Area, Line, Label } from 'recharts'
 
 const data = [
   {
@@ -80,28 +80,31 @@ export function ChartLeadTime() {
   return (
     <Card>
       <CardHeader>
-        <CardTitle>Chart: Lead time for change</CardTitle>
+        <CardTitle>Lead time for change</CardTitle>
         <CardDescription>7 days, 13 hours <span>16%</span></CardDescription>
       </CardHeader>
       <CardContent>
         <ResponsiveContainer width="100%" height={400}>
           <ComposedChart data={data} margin={{ top: 24, right: 48, left: 24, bottom: 0 }}>
             <CartesianGrid vertical={false} stroke="#CFD8DC" />
-            <XAxis label="Calendar date" dataKey="date" />
-            <YAxis label={{ value: 'Completion time (days)', angle: -90, position: 'insideLeft' }} />
+            <XAxis dataKey="date">
+              <Label value="Calendar date" offset={0} position="insideBottom" />
+            </XAxis>
+            <YAxis>
+              <Label value="Completion time (days)" angle={-90} position="insideLeft" style={{ textAnchor: 'middle' }} />
+            </YAxis>
             <Tooltip />
             <Legend layout="vertical" verticalAlign="middle" align="right" />
             <Area type="monotone" dataKey="expected_range" fill="#ECEFF1" stroke="#ECEFF1" />
-            <Line type="monotone" dataKey="goal" dot={false} stroke="#263238" strokeWidth={2} strokeDasharray="2 2" />
+            <Line type="monotone" dataKey="goal" dot={false} stroke="#263238" strokeWidth={2} strokeDasharray="3 3" strokeLinecap="round" />
             <Line type="monotone" dataKey="average" dot={false} stroke="#263238" strokeWidth={3} strokeLinecap="round" />
-            <Line type="monotone" dataKey="rolling_average" dot={false} stroke="#263238" strokeWidth={3} strokeDasharray="4 8" strokeLinecap="round" />
-            
+            <Line type="monotone" dataKey="rolling_average" dot={false} stroke="#263238" strokeWidth={3} strokeDasharray="4 8" strokeLinecap="round" />            
           </ComposedChart>
         </ResponsiveContainer>
       </CardContent>
-      <CardFooter>
+      {/* <CardFooter>
         <p>Card Footer</p>
-      </CardFooter>
+      </CardFooter> */}
     </Card>
   )
 }
