d6c88a81221860f2afc9f8c2f3578b972c3019dc
Mark Riggan <5150241+asuwebdesign@users.noreply.github.com>
2023-08-02 13:47:32 -0400

    Add session data to UserNav

--- components/user-nav.jsx
+++ components/user-nav.jsx
@@ -1,14 +1,18 @@
 'use client'
 
-import { useRouter } from 'next/navigation'
 import { Avatar, AvatarFallback, AvatarImage, } from "@/components/ui/avatar"
 import { Button } from "@/components/ui/button"
 import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuShortcut, DropdownMenuTrigger, } from "@/components/ui/dropdown-menu"
 import { signOut } from 'next-auth/react'
 
-export function UserNav(props) {
+function getInitials(name) {
+  const nameArray = name.split(" ")
+  const firstName = nameArray[0].charAt(0).toUpperCase()
+  const lastName = nameArray[nameArray.length - 1].charAt(0).toUpperCase()
+  return firstName + lastName
+}
 
-  const router = useRouter()
+export function UserNav(props) {
 
   return (
     <DropdownMenu>
@@ -16,7 +20,7 @@ export function UserNav(props) {
         <Button variant="ghost" className="relative h-10 w-10 rounded-full">
           <Avatar className="h-10 w-10">
             <AvatarImage src={props.user.image} alt={`${props.user.name} avatar`} />
-            <AvatarFallback>SC</AvatarFallback>
+            <AvatarFallback className="bg-violet-700 text-white">{getInitials(props.user.name)}</AvatarFallback>
           </Avatar>
         </Button>
       </DropdownMenuTrigger>
@@ -39,7 +43,7 @@ export function UserNav(props) {
           </DropdownMenuItem>
         </DropdownMenuGroup>
         <DropdownMenuSeparator />
-        <DropdownMenuItem onClick={() => signOut({ callbackUrl: '/' })}>
+        <DropdownMenuItem onClick={() => signOut()}>
           Sign out
           <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
         </DropdownMenuItem>
