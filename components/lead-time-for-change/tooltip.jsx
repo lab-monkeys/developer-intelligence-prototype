22e298a2e76bb201d5733ac314244c26ab1fd1b8
Mark Riggan <5150241+asuwebdesign@users.noreply.github.com>
2023-08-03 13:35:31 -0400

    Added color coding to chart tooltips

--- components/lead-time-for-change/tooltip.jsx
+++ components/lead-time-for-change/tooltip.jsx
@@ -7,7 +7,7 @@ export const LeadTimeForChangeTooltip = ({ active, payload, label }) => {
         <div className="flex flex-col gap-2">
           {payload.map((data, index) => (
             <div key={index}>
-              <div className="text-xs text-neutral-600 dark:text-neutral-400">{data.name}</div>
+              <div className="text-xs text-neutral-600 dark:text-neutral-400"><span className="inline-block w-2 h-2 mr-2 rounded-full" style={{ backgroundColor: data.color }}></span>{data.name}</div>
               <div className="text-sm text-black font-semibold dark:text-white">{data.value instanceof Array ? data.value.join('-') : data.value} days</div>              
             </div>
           ))}
