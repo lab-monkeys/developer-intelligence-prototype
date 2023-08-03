3fcb99980a822efbacbfd43879a5dee6d1493dbc
Mark Riggan <5150241+asuwebdesign@users.noreply.github.com>
2023-08-03 09:17:30 -0400

    Dark mode support for chart tooltips

--- components/mean-time-to-recovery/tooltip.jsx
+++ components/mean-time-to-recovery/tooltip.jsx
@@ -2,13 +2,13 @@ export const MeanTimeToRecoveryTooltip = ({ active, payload, label }) => {
 
   if (active && payload && payload.length) {
     return (
-      <div className="flex flex-col gap-4 w-40 bg-white p-4 rounded-2xl shadow">
-        <div className="text-black text-lg font-semibold tracking-tight">{label}</div>
+      <div className="flex flex-col gap-4 w-40 bg-white p-4 rounded-2xl shadow dark:bg-neutral-800">
+        <div className="text-black text-lg font-semibold tracking-tight dark:text-white">{label}</div>
         <div className="flex flex-col gap-2">
           {payload.map((data, index) => (
             <div key={index}>
-              <div className="text-xs text-zinc-600">{data.name}</div>
-              <div className="text-sm text-black font-semibold">{data.value instanceof Array ? data.value.join('-') : data.value}</div>              
+              <div className="text-xs text-neutral-600 dark:text-neutral-400">{data.name}</div>
+              <div className="text-sm text-black font-semibold dark:text-white">{data.value instanceof Array ? data.value.join('-') : data.value}</div>              
             </div>
           ))}
         </div>
