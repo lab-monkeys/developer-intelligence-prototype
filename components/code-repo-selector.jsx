8f50946f9bc3a2f730902935ac06ae951d2b6e7a
Mark Riggan <5150241+asuwebdesign@users.noreply.github.com>
2023-08-04 07:42:43 -0400

    Updated code selector labels/actions

--- components/code-repo-selector.jsx
+++ components/code-repo-selector.jsx
@@ -1,14 +1,14 @@
 "use client"
 
 import { useState } from 'react'
-import { Check, ChevronsUpDown, Github, Gitlab } from "lucide-react"
+import { Check, ChevronsUpDown, Github, Gitlab, Box } from "lucide-react"
 
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, } from "@/components/ui/command"
 import { Popover, PopoverContent, PopoverTrigger, } from "@/components/ui/popover"
 
-const repositories = [
+const applications = [
   {
     value: "psc-ansible",
     label: "psc-ansible",
@@ -33,29 +33,37 @@ export function CodeRepoSelector() {
   return (
     <Popover open={open} onOpenChange={setOpen}>
       <PopoverTrigger asChild>
-        <Button variant="outline" role="combobox" aria-expanded={open} className="w-[200px] justify-between rounded-full">
-          {value && repositories.find((repository) => repository.value === value)?.label}
-          {!value && 'Select repository...'}
+        <Button variant="outline" role="combobox" aria-expanded={open} className="w-64 justify-between rounded-full">
+          <div className="flex items-center">
+            <Box className="mr-2 h-4 w-4 shrink-0" />
+            {value && applications.find((application) => application.value === value)?.label}
+            {!value && 'Select application...'}
+          </div>
           <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
         </Button>
       </PopoverTrigger>
       <PopoverContent align={'left'} className="w-[320px] p-0">
         <Command>
-          <CommandInput placeholder="Search repository..." />
-          <CommandEmpty>No repository found.</CommandEmpty>
+          <CommandInput placeholder="Search application..." />
+          <CommandEmpty>No application found.</CommandEmpty>
           <CommandGroup>
-            {repositories.map((repository) => (
-              <CommandItem key={repository.value} onSelect={(currentValue) => {
+            {applications.map((application) => (
+              <CommandItem className="flex items-center justify-between" key={application.value} onSelect={(currentValue) => {
                 setValue(currentValue === value ? "" : currentValue)
                 setOpen(false)
               }}>
-                <Check className={cn("mr-2 h-4 w-4", value === repository.value ? "opacity-100" : "opacity-0")} />
-                <repository.icon className={cn("mr-2 w-4 h-4", repository.value === value ? "opacity-100" : "opacity-40")} />
-                {repository.label}
+                <div className="flex items-center">
+                  <application.icon className={cn("mr-2 w-4 h-4", application.value === value ? "opacity-100" : "opacity-40")} />
+                  {application.label}
+                </div>
+                <Check className={cn("mr-2 h-4 w-4", value === application.value ? "opacity-100" : "opacity-0")} />
               </CommandItem>
             ))}
           </CommandGroup>
         </Command>
+        <div className="p-2">
+          <Button variant="secondary" className="w-full rounded-full" disabled>Customize</Button>
+        </div>
       </PopoverContent>
     </Popover>
   )
