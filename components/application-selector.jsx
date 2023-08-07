44e25f2c6d87719a3640cf45c548f2c87a186531
Mark Riggan <5150241+asuwebdesign@users.noreply.github.com>
2023-08-07 07:59:20 -0400

    Remove store ref to application selector

--- components/application-selector.jsx
+++ components/application-selector.jsx
@@ -6,7 +6,6 @@ import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, } from "@/components/ui/command"
 import { Popover, PopoverContent, PopoverTrigger, } from "@/components/ui/popover"
-import { useApplicationContext } from '@/app/context/store'
 
 const applications = [
   {
