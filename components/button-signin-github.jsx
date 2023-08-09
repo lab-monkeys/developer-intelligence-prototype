b21e1d8158fd7267343f935a9276af351ca31bb7
Mark Riggan <5150241+asuwebdesign@users.noreply.github.com>
2023-08-09 08:58:09 -0400

    Sign in buttons support loading states

--- components/button-signin-github.jsx
+++ components/button-signin-github.jsx
@@ -1,22 +1,35 @@
 'use client'
 
+import { useState } from 'react'
 import { useTheme } from 'next-themes'
 import { signIn } from 'next-auth/react'
 import { Button } from '@/components/ui/button'
 import LogoGithub from '@/public/logos/logo--github.svg'
 import LogoGitHubDark from '@/public/logos/logo--github--dark.svg'
+import { Loader2 } from "lucide-react"
 
 export default function ButtonSignInGithub() {
 
   const { resolvedTheme } = useTheme()
+  const [loading, setLoading] = useState(false)
+
+  function handleClick() {
+    setLoading(loading => !loading)
+    signIn('github', { callbackUrl: '/dashboard' })
+  }
 
   return (
-    <Button type="button" size="lg" onClick={() => signIn('github', { callbackUrl: '/dashboard' })}>
-      <span className="mr-2 w-4 h-4">
-        {resolvedTheme === 'dark' && <LogoGitHubDark />}
-        {resolvedTheme === 'light' && <LogoGithub />}
-      </span>
-      <span>Sign in with Github</span>
+    <Button type="button" size="lg" onClick={handleClick}>
+      {loading && <Loader2 className="mr-2 h-6 w-6 animate-spin" />}
+      {!loading && (
+        <>
+          <span className="mr-2 w-4 h-4">
+            {resolvedTheme === 'dark' && <LogoGitHubDark />}
+            {resolvedTheme === 'light' && <LogoGithub />}
+          </span>
+          <span>Sign in with Github</span>
+        </>
+      )}
     </Button>
   )
 }
--- components/button-signin-google.jsx
+++ components/button-signin-google.jsx
@@ -1,25 +1,31 @@
 'use client'
 
-import { useRouter } from 'next/navigation'
+import { useState } from 'react'
 import { signIn } from 'next-auth/react'
 import { Button } from '@/components/ui/button'
 import LogoGoogle from '@/public/logos/logo--google.svg'
-
-// Temporarily disable authentication using useRouter. Restore using click function:
-// onClick={() => signIn('google', { callbackUrl: '/dashboard' })}
-// onClick={() => router.push('/dashboard')}
+import { Loader2 } from "lucide-react"
 
 export default function ButtonSignInGoogle() {
 
-  // temporary
-  const router = useRouter()
+  const [loading, setLoading] = useState(false)
+
+  function handleClick() {
+    setLoading(loading => !loading)
+    signIn('google', { callbackUrl: '/dashboard' })
+  }
 
   return (
-    <Button variant="outline" size="lg" type="button" onClick={() => signIn('google', { callbackUrl: '/dashboard' })}>
-      <span className="mr-2 w-4 h-4">
-        <LogoGoogle />
-      </span>
-      <span>Sign in with Google</span>
+    <Button variant="outline" size="lg" type="button" onClick={handleClick}>
+      {loading && <Loader2 className="mr-2 h-6 w-6 animate-spin" />}
+      {!loading && (
+        <>
+          <span className="mr-2 w-4 h-4">
+            <LogoGoogle />
+          </span>
+          <span>Sign in with Google</span>
+        </>
+      )}
     </Button>
   )
 }
