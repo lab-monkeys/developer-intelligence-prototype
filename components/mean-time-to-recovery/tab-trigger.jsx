16fe8760c8e02fe8e0fbaf6df475242b50762c27
Eric Sauer <esauer@redhat.com>
2024-02-07 23:47:58 +0000

    Update to pass a timestamp along with a date range to the api call

--- components/mean-time-to-recovery/tab-trigger.jsx
+++ components/mean-time-to-recovery/tab-trigger.jsx
@@ -25,7 +25,7 @@ export function MeanTimeToRecoveryTabTrigger({ dateRange, data, appName }) {
   const [isLoading, setLoading] = useState(true)
 
   useEffect(() => {
-    fetch(`${process.env.NEXT_PUBLIC_PELORUS_API_URL}/sdp/mean_time_to_restore/${appName}?range=${getDaysBetweenDates(dateRange)}d`)
+    fetch(`${process.env.NEXT_PUBLIC_PELORUS_API_URL}/sdp/mean_time_to_restore/${appName}?range=${getDaysBetweenDates(dateRange)}d&start=${dateRange.to.getTime() / 1000}`)
       .then((test) => test.json())
       .then((response) => {
         setResponse(response)
