7327e68550369ceae19cdd3dc2c9b6ebd44ef159
Eric Sauer <esauer@redhat.com>
2024-08-06 14:47:13 +0000

    Add a local user/pass mechanism that will only redner for local development

--- app/api/auth/[...nextauth]/options.js
+++ app/api/auth/[...nextauth]/options.js
@@ -1,7 +1,5 @@
 import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
-import GoogleProvider from "next-auth/providers/google"
-import KeycloakProvider from "next-auth/providers/keycloak";
+import CredentialsProvider from "next-auth/providers/credentials"
 
 async function makeUserinfoRequest(context) {
   console.log(context)
@@ -19,51 +17,61 @@ async function makeUserinfoRequest(context) {
   return res.json()
 }
 
+const providers = [
+  {
+    id: "openshift",
+    name: "OpenShift",
+    type: "oauth",
+    wellKnown: "https://openshift.default.svc/.well-known/oauth-authorization-server",
+    authorization: { params: { scope: "user:info" } },
+    userinfo: {
+      url: "https://kubernetes.default.svc/apis/user.openshift.io/v1/users/~",
+      // The result of this method will be the input to the `profile` callback.
+      async request(context) {
+        // context contains useful properties to help you make the request.
+        return await makeUserinfoRequest(context)
+      }
+    },
+    profile(profile) {
+      return {
+        id: profile.metadata.uid,
+        username: profile.metadata.name,
+        name: profile.metadata.name,
+        email: profile.metadata.name,
+        groups: profile.groups
+      }
+    },
+    clientId: process.env.OPENSHIFT_CLIENT_ID, // this should be a service account with name format system:serviceaccount:<namespace>:<serviceaccount name>
+    clientSecret: process.env.OPENSHIFT_CLIENT_SECRET // this is the service account token, which can be extracted from a secret
+  }
+]
+
+// Conditionally add the basic auth provider in development mode
+if (process.env.NODE_ENV === "development") {
+  providers.push(
+    CredentialsProvider({
+      id: "local",
+      name: "Local",
+      credentials: {
+        username: { label: "Username", type: "text" },
+        password: { label: "Password", type: "password" }
+      },
+      authorize(credentials) {
+        if (credentials.username === "pelorus" && credentials.password === "pelorus") {
+          return { id: 1, name: "pelorus", email: "pelorus@example.com" }
+        }
+        return null
+      }
+    })
+  )
+}
+
 export const options = {
-  providers: [
-    {
-      id: "openshift",
-      name: "OpenShift",
-      type: "oauth",
-      wellKnown: "https://openshift.default.svc/.well-known/oauth-authorization-server",
-      authorization: { params: { scope: "user:info" } },
-      userinfo: {
-        url: "https://kubernetes.default.svc/apis/user.openshift.io/v1/users/~",
-        // The result of this method will be the input to the `profile` callback.
-        async request(context) {
-          // context contains useful properties to help you make the request.
-          return await makeUserinfoRequest(context)
-        }
-      },
-      profile(profile) {
-        return {
-          id: profile.metadata.uid,
-          username: profile.metadata.name,
-          name: profile.metadata.name,
-          email: profile.metadata.name,
-          groups: profile.groups
-        }
-      },
-      clientId: process.env.OPENSHIFT_CLIENT_ID, // this should be a service account with name format system:serviceaccount:<namespace>:<serviceaccount name>
-      clientSecret: process.env.OPENSHIFT_CLIENT_SECRET // this is the service account token, which can be extracted from a secret
-    }
-    // KeycloakProvider({
-    //   clientId: process.env.KEYCLOAK_ID,
-    //   clientSecret: process.env.KEYCLOAK_SECRET,
-    //   issuer: process.env.KEYCLOAK_ISSUER,
-    // }),
-    // GithubProvider({
-    //   clientId: process.env.GITHUB_ID,
-    //   clientSecret: process.env.GITHUB_SECRET,
-    // }),
-    // GoogleProvider({
-    //   clientId: process.env.GOOGLE_CLIENT_ID,
-    //   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    // })
-  ],
+  providers,
   pages: {
     signIn: '/',
     signOut: '/',
   }
 }
+
 export default NextAuth(options)
--- app/api/old-api.jsx
+++ app/api/old-api.jsx
@@ -7,9 +7,9 @@ export async function getData() {
   }
 
 export async function getApps() {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_PELORUS_API_URL}/sdp/apps?range=1w`)
-    if (!response.ok) {
-      throw new Error('Failed to fetch list of Apps from Pelorus')
-    }
-    return response.json()
+  const response = await fetch(`${process.env.NEXT_PUBLIC_PELORUS_API_URL}/sdp/apps?range=1w`)
+  if (!response.ok) {
+    throw new Error(`Failed to fetch list of Apps from Pelorus` + response.statusText)
   }
+  return response.json()
+}
